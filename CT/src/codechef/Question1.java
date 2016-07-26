c514ca61730410852a697d50f8a7524c58cd1026
rahul <rahul.george.in@snapdeal.com>
2016-07-26 14:54:32 +0530

    Minor changes

--- CT/src/codechef/Question1.java
+++ CT/src/codechef/Question1.java
@@ -7,70 +7,57 @@ import java.util.Scanner;
 public class Question1 {
 	static int b1;
 	static int b2;
-    static long inc;
-    
-	static int solve(long no, int index, int count) {
+	static int index;
+	static int count;
+	static int incr;
 
-		if (no < Math.pow(2, 60)) {
-			if (palindrome(new BigInteger(Long.toString(no, b1)))
-					&& palindrome(new BigInteger(Long.toString(no, b2)))) {
+	static int solve() {
+		incr = Math.min(b1, b2);
+		for ( BigInteger no = new BigInteger("1");no.compareTo(BigInteger.valueOf((long) Math.pow(2, 60)))==-1 ;no=no.add(new BigInteger("1")) ) {
+			/*
+			if (palindrome(no.toString(b1))
+					&& palindrome(no.toString(b2))) {
 				System.out.println("Palindrome " + no);
-                     ++count;
-				if (count > 1000) 
+				++count;
+				if (count <= 1000) {
+					result[index++] = no.longValue();
+				} else
 					return 1000;
-			   result[index++] = no;
-                    
-				inc=no;
 			}
-			else
-				inc=Math.min(b1, b2);
-			return count+solve(no+inc,index,count);
+			*/
 		}
-		
 		return count;
 	}
 
-	private static boolean palindrome(BigInteger bigInteger) {
+	private static boolean palindrome(String bigInteger) {
 		// TODO Auto-generated method stub
-		BigInteger x;
-		BigInteger r = new BigInteger("0");
-		BigInteger tmp = bigInteger;
-		while (!bigInteger.equals(new BigInteger("0"))) {
-			x = bigInteger.mod(new BigInteger("10"));
-			r = r.multiply(new BigInteger("10")).add(x);
-			bigInteger = bigInteger.divide(new BigInteger("10"));
-		}
-
-		if (r.equals(tmp))
+		String rev=new StringBuilder(bigInteger).reverse().toString();
+		if(rev.compareTo(bigInteger)==0)
 			return true;
-
 		return false;
-
 	}
 
 	static long result[];
 	private static Scanner sc;
 
 	public static void main(String args[]) {
-
 		sc = new Scanner(System.in);
 		int no;
-		
-
 		no = sc.nextInt();
 		for (int i = 0; i < no; i++) {
 			result = new long[1000];
 			b1 = sc.nextInt();
 			b2 = sc.nextInt();
-			if (solve(1, 0, 0) > 0)
+			index = 0;
+			count = 0;
+			if (solve() > 0)
 				display(result);
-
-		}
+           }
 	}
 
 	private static void display(long[] result) {
 		// TODO Auto-generated method stub
-		for (int i = 0; i < 1000 && result[i] != 0; i++)
+		for (int i = 0; i < (count > 1000 ? count - 1 : count); i++)
 			System.out.println(result[i]);
 	}
 }
