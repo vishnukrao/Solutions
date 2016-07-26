de8a9ee12d26e5aa10fa5addb542926dccf6b4bd
rahul <rahul.george.in@snapdeal.com>
2016-07-26 11:01:49 +0530

    Recursive

--- CT/src/codechef/Question1.java
+++ CT/src/codechef/Question1.java
@@ -5,72 +5,72 @@ import java.util.Scanner;
 
 // Chef and dual palindromes
 public class Question1 {
+	static int b1;
+	static int b2;
+    static long inc;
+    
+	static int solve(long no, int index, int count) {
 
-	static int solve(int b1,int b2)
-	{
-		int count = 0,index=0;
-		for(long i=11;i<Math.pow(2,20);i++)
-		{
-			
-				if(palindrome(new BigInteger(Long.toString(i,b1))) && palindrome(new BigInteger(Long.toString(i,b2))))
-			     {
-					System.out.println("Palindrome "+i);
-
-					  ++count;
-					  if(count<=1000)
-			           result[index++]=i;
-					  else
-						  break;
-			    
-			     }
+		if (no < Math.pow(2, 60)) {
+			if (palindrome(new BigInteger(Long.toString(no, b1)))
+					&& palindrome(new BigInteger(Long.toString(no, b2)))) {
+				System.out.println("Palindrome " + no);
+                     ++count;
+				if (count > 1000) 
+					return 1000;
+			   result[index++] = no;
+                    
+				inc=no;
+			}
+			else
+				inc=Math.min(b1, b2);
+			return count+solve(no+inc,index,count);
 		}
-		if(count<=1000)
-			return 0;
-		return 1000;
+		
+		return count;
 	}
-	
+
 	private static boolean palindrome(BigInteger bigInteger) {
 		// TODO Auto-generated method stub
 		BigInteger x;
 		BigInteger r = new BigInteger("0");
-		BigInteger tmp=bigInteger;
-		while(!bigInteger.equals(new BigInteger("0")))
-		{
-			x=bigInteger.mod(new BigInteger("10"));
-			r=r.multiply(new BigInteger("10")).add(x);
-			bigInteger=bigInteger.divide(new BigInteger("10"));
+		BigInteger tmp = bigInteger;
+		while (!bigInteger.equals(new BigInteger("0"))) {
+			x = bigInteger.mod(new BigInteger("10"));
+			r = r.multiply(new BigInteger("10")).add(x);
+			bigInteger = bigInteger.divide(new BigInteger("10"));
 		}
-		 
-        
-		if(r.equals(tmp))
-			
 
+		if (r.equals(tmp))
 			return true;
-		
+
 		return false;
-		
+
 	}
 
 	static long result[];
 	private static Scanner sc;
-	public static void main(String args[])
-	{
-		
+
+	public static void main(String args[]) {
+
 		sc = new Scanner(System.in);
 		int no;
 		
-		no=sc.nextInt();
-		for(int i=0;i<no;i++)
-		{
-			result=new long[1000];
-			if(solve(sc.nextInt(),sc.nextInt())>0)
+
+		no = sc.nextInt();
+		for (int i = 0; i < no; i++) {
+			result = new long[1000];
+			b1 = sc.nextInt();
+			b2 = sc.nextInt();
+			if (solve(1, 0, 0) > 0)
 				display(result);
-				
+
 		}
 	}
-	private static void display(long[] result2) {
+
+	private static void display(long[] result) {
 		// TODO Auto-generated method stub
-		for(int i=0;i<1000;i++)
-			System.out.println(result2[i]);
+		for (int i = 0; i < 1000 && result[i] != 0; i++)
+			System.out.println(result[i]);
 	}
 }
